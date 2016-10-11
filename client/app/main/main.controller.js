56bc790e93f0198f67046b229a7d5ff7567ddb79
mrubalcava <miguel.rubalcava@FSANPABLO.onmicrosoft.com>
2016-10-11 09:45:14 -0500

    calendar

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,11 +4,15 @@
 
   class MainController {
 
-    constructor($http, $scope, socket) {
+    constructor($http, $scope, socket, $mdDialog) {
       this.$http = $http;
       this.socket = socket;
       this.awesomeThings = [];
+      this.eventSources = [];
+      this.events = [];
       this.uiConfig = {calendar:{}};
+      this.mdDialog = $mdDialog;
+      this.test = 'hello mundo';
       $scope.$on('$destroy', function() {
         socket.unsyncUpdates('thing');
       });
@@ -39,11 +43,84 @@
               left: 'month agendaWeek agendaDay',
               center: 'title',
               right: 'today prev,next'
+            },
+            eventClick: function() {
+              console.log('wooo');
+
             }
           }
         };
 
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+         this.events = [
+          {title: 'All Day Event',start: new Date(y, m, 1)},
+          {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+          {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+          {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+          {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+          {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+        ];
+
+         this.eventSources = [this.events];
+
     }
+
+    addAppointment(data){
+      var date = new Date();
+      var d = date.getDate();
+      var m = date.getMonth();
+      var y = date.getFullYear();
+      this.events.push({
+         title: 'Open Sesame',
+         start: new Date(y, m, d),
+         end: new Date(y, m, d),
+         className: ['openSesame']
+       });
+
+    }
+
+    createAppointment(){
+
+      var data = {};
+      console.log('creating appointment');
+      this.mdDialog.show({
+      onRemoving : function(){
+        console.log('ejecutando on removing');
+        //$scope.orderUpdate(data);
+      },
+      onComplete : function(){
+//				$('#modalOrder').addClass('animated shake');
+      },
+      escapeToClose : true,
+      //scope: $scope,
+      //preserveScope: false,
+      clickOutsideToClose: true,
+      bindToController: true,
+      disableParentScroll: true,
+
+      focusOnOpen: false,
+      fullscreen: true,
+      //targetEvent: event,
+      controller: 'appointmentCtrl',
+      //controllerAs: 'ctrl',
+      templateUrl: 'app/main/appointment/appointment.html',
+      locals : {
+        data : data
+      }
+      //disableParentScroll: true
+    }).then(function(data) {
+        //we  save the order only if the promise was succesfully
+        console.log(data);
+        this.addAppointment(data);
+      }.bind(this));
+
+
+
+    }
+
 
     addThing() {
       if (this.newThing) {
