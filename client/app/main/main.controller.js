354c769e35485cd150fb136a35aa646953befa2d
mrubalcava <miguel.rubalcava@FSANPABLO.onmicrosoft.com>
2016-10-18 17:42:03 -0500

    create appoiment

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -10,12 +10,46 @@
       this.awesomeThings = [];
       this.eventSources = [];
       this.events = [];
-      this.uiConfig = {calendar:{}};
+      //this.uiConfig = {calendar:{}};
       this.mdDialog = $mdDialog;
       this.test = 'hello mundo';
       $scope.$on('$destroy', function() {
         socket.unsyncUpdates('thing');
       });
+      //lets initalize the main configuration
+      this.uiConfig = {
+        calendar:{
+          locale: 'es',
+          dayNames : ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+          monthNames : ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio', 'Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+          dayNamesShort : ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+          buttonText: {
+            today:'Hoy',
+            month: 'Mes',
+            week: 'Semana',
+            day: 'Dia'
+          },
+          editable: true,
+          header:{
+            left: 'month agendaWeek agendaDay',
+            center: 'title',
+            right: 'today prev,next'
+          },
+          businessHours:{
+            // days of week. an array of zero-based day of week integers (0=Sunday)
+            dow: [ 1, 2, 3, 4, 5], // Monday - Thursday
+            start: '9:00', // a start time (10am in this example)
+            end: '13:30'// an end time (6pm in this example)
+          },
+          eventClick: function() {
+            console.log('wooo');
+
+          },
+          dayClick: function(date){
+            return this.createAppointment(date);
+          }.bind(this)
+        }
+      };
     }
 
     $onInit() {
@@ -25,43 +59,19 @@
           this.socket.syncUpdates('thing', this.awesomeThings);
         });
 
-        //lets initalize the main configuration
-        this.uiConfig = {
-          calendar:{
-            locale: 'es',
-            dayNames : ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-            monthNames : ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio', 'Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-            dayNamesShort : ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
-            buttonText: {
-              today:'Hoy',
-              month: 'Mes',
-              week: 'Semana',
-              day: 'Dia'
-            },
-            editable: true,
-            header:{
-              left: 'month agendaWeek agendaDay',
-              center: 'title',
-              right: 'today prev,next'
-            },
-            eventClick: function() {
-              console.log('wooo');
 
-            }
-          }
-        };
 
         var date = new Date();
         var d = date.getDate();
         var m = date.getMonth();
         var y = date.getFullYear();
          this.events = [
-          {title: 'All Day Event',start: new Date(y, m, 1)},
-          {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-          {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-          {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-          {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-          {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+          // {title: 'All Day Event',start: new Date(y, m, 1)},
+          // {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+          // {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+          // {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+          // {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+          {title: 'Manuel Orozco',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
         ];
 
          this.eventSources = [this.events];
@@ -69,22 +79,15 @@
     }
 
     addAppointment(data){
-      var date = new Date();
-      var d = date.getDate();
-      var m = date.getMonth();
-      var y = date.getFullYear();
-      this.events.push({
-         title: 'Open Sesame',
-         start: new Date(y, m, d),
-         end: new Date(y, m, d),
-         className: ['openSesame']
-       });
+      // var date = new Date();
+      // var d = date.getDate();
+      // var m = date.getMonth();
+      // var y = date.getFullYear();
+      this.events.push(data);
 
     }
 
-    createAppointment(){
-
-      var data = {};
+    createAppointment(date){
       console.log('creating appointment');
       this.mdDialog.show({
       onRemoving : function(){
@@ -108,17 +111,13 @@
       //controllerAs: 'ctrl',
       templateUrl: 'app/main/appointment/appointment.html',
       locals : {
-        data : data
+        data : date
       }
       //disableParentScroll: true
     }).then(function(data) {
         //we  save the order only if the promise was succesfully
-        console.log(data);
         this.addAppointment(data);
       }.bind(this));
-
-
-
     }
 
 
